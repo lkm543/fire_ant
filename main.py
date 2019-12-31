e9a49dbf6de7e092ac94dc94c7928d013871edcb
lkm543 <lkm543@gmail.com>
2019-12-31 17:24:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class Frame(wx.App):
         self.mainSizer.Add(self.imageCtrl_l, 0, wx.ALL, 5)
         self.mainSizer.Add(self.imageCtrl_2, 0, wx.ALL, 5)
         self.sizer.Add(self.photoTxt, 0, wx.ALL, 5)
-        self.sizer.Add(browseBtn, 0, wx.ALL, 5)        
+        self.sizer.Add(browseBtn, 0, wx.ALL, 5)
         self.mainSizer.Add(self.sizer, 0, wx.ALL, 5)
  
         self.panel.SetSizer(self.mainSizer)
