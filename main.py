4b875885c7525a56988a291b6becb12eec41a6e7
lkm543 <lkm543@gmail.com>
2019-12-31 16:58:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import wx
-from skimage import io
 import cv2
 import numpy as np
+import wx
+from skimage import io
+
+from segmentation import image_process
 
 
 # http://www.blog.pythonlibrary.org/2010/03/26/creating-a-simple-photo-viewer-with-wxpython/
@@ -79,101 +81,15 @@ class Frame(wx.App):
     def load_image(self):
         filepath = self.photoTxt.GetValue()
         image = io.imread(filepath)
-        image = self.segmentation(image)
+        print(type(image))
+        image = image_process.segmentation(image)
+        print(type(image))
         height, width, nrgb = image.shape
-        wximg = wx.ImageFromBuffer(width, height, image)
+        print(height, width, nrgb)
+        wximg = wx.Bitmap.FromBuffer(width, height, image)
         save_filename = self.photoTxt.GetValue().split('\\')[-1]
         self.save_image(wx.Bitmap(wximg), save_filename)
         return wximg
-
-    def segmentation(self, image_ndarray):
-        '''
-        # Graph Based Segmentation 
-        segmentator = cv2.ximgproc.segmentation.createGraphSegmentation(sigma=1, k=30, min_size=500)
-        segment = segmentator.processImage(image_ndarray)
-        seg_image = np.zeros(image_ndarray.shape, np.uint8)
-        for i in range(np.max(segment)):
-            # 將第 i 個分割的座標取出
-            y, x = np.where(segment == i)
-
-        # 隨機產生顏色
-        color = [random.randint(0, 255), random.randint(0, 255),random.randint(0, 255)]
-
-        # 設定第 i 個分割區的顏色
-        for xi, yi in zip(x, y):
-            seg_image[yi, xi] = color
-
-        # 將原始圖片與分割區顏色合併
-        result = cv2.addWeighted(image_ndarray, 0.3, seg_image, 0.7, 0)
-        '''
-
-        '''
-        # Convert to Gray and threshold, erode and dilate
-        gray = cv2.cvtColor(image_ndarray, cv2.COLOR_BGR2GRAY)
-        cv2.normalize(gray, gray, 0, 255, cv2.NORM_MINMAX)
-        ret, otsu_mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        binary = cv2.erode(otsu_mask, None, iterations=45)
-        binary = cv2.dilate(binary, None, iterations=120)
-        binary = cv2.erode(binary, None, iterations=75)
-        # binary = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,111,21)
-        result = image_ndarray.copy()
-        masked = cv2.bitwise_and(image_ndarray, image_ndarray, mask=binary)
-        result = cv2.addWeighted(image_ndarray, 0.35, masked, 0.65, 0)
-        '''
-
-        # find contours
-        gray = cv2.cvtColor(image_ndarray, cv2.COLOR_BGR2GRAY)
-        cv2.normalize(gray, gray, 0, 255, cv2.NORM_MINMAX)
-        ret, otsu_mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        binary = cv2.erode(otsu_mask, None, iterations=30)
-        binary = cv2.dilate(binary, None, iterations=30)
-        (cnts,_) = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        area = np.array([cv2.contourArea(cnts[i]) for i in range(len(cnts))])
-        maxa_ind = np.argmax(area)
-        hull = cv2.convexHull(cnts[maxa_ind], False)
-        result = image_ndarray.copy()
-
-        length = len(hull)
-        for i in range(len(hull)):
-            cv2.line(result, tuple(hull[i][0]), tuple(hull[(i+1)%length][0]), (0,255,0), 10)
-
-        '''
-        hsv = cv2.cvtColor(image_ndarray, cv2.COLOR_BGR2HSV)
-        # print(hsv)
-        center_hue, center_saturation, center_value = self.get_center_color(image_ndarray)
-        gray = cv2.cvtColor(image_ndarray, cv2.COLOR_RGB2GRAY)
-        kernel_size = 3
-        gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size), 0)
-        mask = cv2.Canny(gray, 30, 255)
-        # cv2.imshow('Edges', edges)
-        # print(f'Select Hue: {center_hue}')
-        '''
-
-        '''
-        row, col, channel = image_ndarray.shape
-        ret, otsu_mask = cv2.threshold(hsv[:,:,2], 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        mask = np.zeros((row, col, 1), np.uint8)
-        '''
-        '''
-        for i in range(row):
-            for j in range(col):
-                if center_value-self.threshold < hsv[i,j,2] < center_value+self.threshold:
-                    mask[i,j] = 1
-        '''
-        # masked = cv2.bitwise_and(image_ndarray, image_ndarray, mask=mask)
-        # result = cv2.addWeighted(image_ndarray, 0.2, masked, 0.8, 0)
-
-        '''
-        # Canny and find contour
-        gray = cv2.cvtColor(image_ndarray, cv2.COLOR_BGR2GRAY)
-        cv2.normalize(gray, gray, 0, 255, cv2.NORM_MINMAX)
-        canny = cv2.Canny(gray, 30, 150)
-        cnts = cv2.findContours(canny.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        result = image_ndarray.copy()
-        cv2.drawContours(result, cnts, -1, (0, 255, 0), 2)
-        '''
-
-        return result
 
     def get_center_color(self, image_ndarray):
         row, col, channel = image_ndarray.shape
